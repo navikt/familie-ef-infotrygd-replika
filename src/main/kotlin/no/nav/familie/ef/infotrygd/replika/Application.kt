90feee41a5f3e56de639ce3786575f85128a700c
olekvernberg <ole.christian.kvernberg@nav.no>
2026-06-30 14:23:36 +0200

    Dev config

---
+++ src/main/kotlin/no/nav/familie/ef/infotrygd/replika/Application.kt
@@ -0,0 +1,11 @@
+package no.nav.familie.ef.infotrygd.replika
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+open class Application
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
+}
---
+++ src/main/kotlin/no/nav/familie/ef/infotrygd/replika/config/ApplicationConfig.kt
@@ -0,0 +1,6 @@
+package no.nav.familie.ef.infotrygd.replika.config
+
+import org.springframework.boot.SpringBootConfiguration
+
+@SpringBootConfiguration
+class ApplicationConfig
